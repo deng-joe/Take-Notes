c470560021c34e517cc5cb6062d9913f0007d994
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-05-21 23:45:05 +0300

    Write UI tests

---
+++ app/src/androidTest/java/com/joey/takenotes/MainActivityTest.kt
@@ -0,0 +1,26 @@
+package com.joey.takenotes
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import com.joey.takenotes.ui.MainActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class MainActivityTest {
+
+    @get:Rule
+    var mainRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun checkViewDisplay() {
+        onView(withText(R.string.notes_not_available_yet)).check(matches(isDisplayed()))
+        onView(withId(R.id.fab)).check(matches(isDisplayed()))
+    }
+}
---
+++ app/src/androidTest/java/com/joey/takenotes/NewNoteActivityTest.kt
@@ -0,0 +1,27 @@
+package com.joey.takenotes
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import com.joey.takenotes.ui.NewNoteActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class NewNoteActivityTest {
+
+    @get:Rule
+    var newNoteRule: ActivityTestRule<NewNoteActivity> = ActivityTestRule(NewNoteActivity::class.java)
+
+    @Test
+    fun checkViewDisplay() {
+        onView(withId(R.id.title)).check(matches(isDisplayed()))
+        onView(withId(R.id.body)).check(matches(isDisplayed()))
+    }
+}
